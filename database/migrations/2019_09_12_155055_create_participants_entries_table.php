20ae52f3101878cae6ab0e8aab033f9676d9dc2b
Kapil Paul <kapil@webable.com.bd>
2019-09-12 17:00:17 +0600

    participants_entries table created

---
+++ database/migrations/2019_09_12_155055_create_participants_entries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants_entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('participant_id');
+            $table->integer('guests')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants_entries');
+    }
+}
